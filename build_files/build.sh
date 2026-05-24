d0b1a244360bded76d147b2780bc76fe71a3ac91
attilak <attilak@gmail.com>
2026-05-24 11:52:34 +0300

    Add programs

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,11 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Standard packages from Fedora / RPMFusion repos
+dnf5 install -y \
+    tmux \
+    neovim \
+    mc
 
 # Use a COPR Example:
 #
@@ -18,6 +21,23 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+# Vivaldi
+dnf5 config-manager addrepo --from-repofile=https://repo.vivaldi.com/stable/vivaldi-fedora.repo
+dnf5 install -y vivaldi-stable
+
+# Google Chrome (direct RPM — repo is self-updating once installed)
+dnf5 install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
+# Visual Studio Code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf5 install -y code
+
+# Bitwarden desktop (direct RPM from GitHub releases)
+BITWARDEN_RPM_URL=$(curl -sSL https://api.github.com/repos/bitwarden/clients/releases \
+    | grep -o 'https://[^"]*x86_64\.rpm' | grep '/desktop-' | head -1)
+dnf5 install -y "${BITWARDEN_RPM_URL}"
 
 #### Example for enabling a System Unit File
 
